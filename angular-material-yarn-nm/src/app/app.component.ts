76c357f3b0135ede29560b7bf4c36c71d28846d6
Daniel Gerhardt <code@dgerhardt.net>
2021-12-20 16:44:50 +0100

    Import reproduction

---
+++ angular-material-yarn-nm/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular13.1-yarn3.2-pnp-sass-loader-repro';
+}
